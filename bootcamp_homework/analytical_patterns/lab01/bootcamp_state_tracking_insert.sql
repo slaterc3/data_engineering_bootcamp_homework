1db14cd407535e060be3f1d34000897013773a00
Christopher Guy Slater <73452296+slaterc3@users.noreply.github.com>
2025-02-13 20:21:26 -0500

    Update bootcamp_state_tracking_insert.sql
    incrementing by one day from start

--- bootcamp_homework/analytical_patterns/lab01/bootcamp_state_tracking_insert.sql
+++ bootcamp_homework/analytical_patterns/lab01/bootcamp_state_tracking_insert.sql
@@ -1,13 +1,14 @@
-INSERT INTO slaterc3.bootcamp_state_tracking 
+ INSERT INTO slaterc3.bootcamp_state_tracking 
+ 
  WITH yesterday AS (
    SELECT * 
    FROM slaterc3.bootcamp_state_tracking
-     WHERE ds = DATE('2022-12-31')
+     WHERE ds = DATE('2023-01-01')
  ),
  today AS (
  SELECT * 
  FROM slaterc3.bootcamp_web_events_daily
-   WHERE ds = DATE('2023-01-01')
+   WHERE ds = DATE('2023-01-02')
  ), 
  combined AS (
    SELECT 
@@ -16,6 +17,14 @@ INSERT INTO slaterc3.bootcamp_state_tracking
    COALESCE(t.ds, y.last_active_date) AS last_active_date,
    CASE 
      WHEN y.user_id IS NULL THEN 'new'
+     WHEN y.current_state IN ('new', 'resurrected', 'retained')
+       AND t.user_id IS NULL THEN 'churned'
+     WHEN y.current_state IN ('new', 'resurrected', 'retained') 
+       AND t.user_id IS NOT NULL THEN 'retained'
+     WHEN y.current_state IN ('stale', 'churned') 
+       AND t.user_id IS NOT NULL THEN 'resurrected'
+       WHEN y.current_state IN ('churned') 
+       AND t.user_id IS NULL THEN 'stale'
      ELSE 'unknown'
    END AS current_state,
    COALESCE(lifetime_page_visits, 0) + COALESCE(t.total_page_visits, 0) AS lifetime_page_visits,
@@ -25,3 +34,32 @@ INSERT INTO slaterc3.bootcamp_state_tracking
      ON t.user_id = y.user_id
  )
  SELECT * FROM combined
+-- INSERT INTO slaterc3.bootcamp_state_tracking 
+--  WITH yesterday AS (
+--    SELECT * 
+--    FROM slaterc3.bootcamp_state_tracking
+--      WHERE ds = DATE('2022-12-31')
+--  ),
+--  today AS (
+--  SELECT * 
+--  FROM slaterc3.bootcamp_web_events_daily
+--    WHERE ds = DATE('2023-01-01')
+--  ), 
+--  combined AS (
+--    SELECT 
+--       COALESCE(t.user_id, y.user_id) AS user_id,
+--    COALESCE(y.first_active_date, t.ds) AS first_active_date,
+--    COALESCE(t.ds, y.last_active_date) AS last_active_date,
+--    CASE 
+--      WHEN y.user_id IS NULL THEN 'new'
+--      ELSE 'unknown'
+--    END AS current_state,
+--    COALESCE(lifetime_page_visits, 0) + COALESCE(t.total_page_visits, 0) AS lifetime_page_visits,
+--    DATE('2023-01-01') as ds 
+--    FROM today t 
+--    FULL OUTER JOIN yesterday y 
+--      ON t.user_id = y.user_id
+--  )
+--  SELECT * FROM combined
+
+
