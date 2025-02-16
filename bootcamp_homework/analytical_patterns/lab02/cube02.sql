1851b3b778c9761544b32674a659a00d4eb8e276
Christopher Guy Slater <73452296+slaterc3@users.noreply.github.com>
2025-02-15 19:06:07 -0500

    demonstration of CUBE

---
+++ bootcamp_homework/analytical_patterns/lab02/cube02.sql
@@ -0,0 +1,37 @@
+WITH mapped AS (
+SELECT 
+CASE
+  WHEN host LIKE '%techcreator%' THEN 'TechCreator.io'
+  WHEN host LIKE '%eczachly%' THEN 'EcZachly.com'
+  WHEN host LIKE '%zachwilson%' THEN 'ZachWilson.tech'
+  WHEN host LIKE '%dataengineer%' THEN 'DataEngineer.io'
+  ELSE host
+END AS host,
+BROWSER_TYPE, 
+CASE WHEN DEVICE_TYPE LIKE '%amsung%' THEN 'Samsung'
+ELSE COALESCE(device_type, 'unknown') 
+END AS device_type, 
+COALESCE(OS_TYPe, 'unknown') AS os_type,
+url,
+COUNT(1)
+FROM bootcamp.web_events e
+JOIN bootcamp.devices d 
+  ON d.device_id = e.device_id
+GROUP BY 1, 2, 3, 4,5
+),
+agged AS (
+SELECT 
+  COALESCE(host, '(overall)') AS host, 
+  COALESCE(browser_type, '(overall)') AS browser_type, 
+  COALESCE(device_type, '(overall)') AS device_type, 
+  COALESCE(os_type, '(overall)') AS os_type, 
+  COUNT(1) as page_hits
+FROM mapped
+GROUP BY CUBE (
+      host, browser_type, device_type, os_type
+      )
+    ORDER BY COUNT(1) DESC
+)
+SELECT * from agged
+--COUNT(1) as combo_count FROM agged
+ 
